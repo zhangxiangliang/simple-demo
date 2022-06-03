85f8732b654425cb172c777aee7d214e80fa2953
zhangxiangliang <admin@pushme.top>
2022-06-03 15:31:13 +0800

    feat: 增加创建运行方法，生命周期

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-import { connectDatabase, disconnectDatabase } from './database/index.js';
+import { connectDatabase, disconnectDatabase, createRaw } from './database/index.js';
 
 const main = () => {
-  connectDatabase();
-  disconnectDatabase();
+  connectDatabase(
+    () => console.log('在创建数据库的时候，我要做某件事情'),
+    () => console.log('在运行数据库的时候，我要做某件事情'),
+  );
+
+  try {
+    createRaw('books', { id: 3, title: 'Hello world', author: "Mi" });
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  disconnectDatabase(() => console.log('在关闭数据库的时候，我要做某件事情'));
 }
 
 main();
